fed5ff85be8bf4f2fd305ab4fa6cf02c383bfc1f
ignacio tosini <ignaciotosini2002@gmail.com>
2025-01-08 17:36:55 -0300

    Add documentation Swagger

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -1,15 +1,5 @@
-import request from 'supertest'
-import server, { connectDB } from '../server'
+import { connectDB } from '../server'
 import db from '../config/db'
-
-describe('GET /api', () => {
-    it('should send back a json response', async () => {
-        const res = await request(server).get('/api')
-        expect(res.headers['content-type']).toMatch(/json/)
-        expect(res.status).not.toBe(404)
-        expect(res.body.msg).not.toBe('desde api')
-    })
-})
 
 jest.mock('../config/db')
 
