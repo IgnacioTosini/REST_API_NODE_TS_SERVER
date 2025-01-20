4c2575fbe5d253f50235b3b512c89eed16cc5c09
ignacio tosini <ignaciotosini2002@gmail.com>
2025-01-20 13:59:07 -0300

    posible arreglo render 2

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,13 @@ dotenv.config()
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
     models: [__dirname + '/../models/**/*'],
-    logging: false
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 })
 
 export default db
